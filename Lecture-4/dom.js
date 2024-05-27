aa587957c85bc3aabc2bf6cfa679828a5909ab9e
Pankaj Bhardwaj <pbhardwaj@beesolvertechnology.com>
2024-05-27 16:41:38 +0530

    DOM

---
+++ Lecture-4/dom.js
@@ -0,0 +1,8 @@
+/*=========  DOM ==========
+ Document object model = DOM is a tree like structure which will place all HTML elements in a tree.
+**we can easily traverse throghu different nodes if we have tree like structure.
+** DOM model is reposinsible to add interactivity on the website.
+*/
+
+
+
